5fa97d0913951b8289cc8aac9f5c5bcb536ffb63
Carlos Caicedo <carloscaicedos@gmail.com>
2016-05-21 09:24:21 -0500

    Ejemplo de creación de instancia de una clase por medio de Reflection.

---
+++ Reflection/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Book
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string isbn { get; set; }
+        public int Books_Id { get; set; }
+
+        public Book(string ID, string Title, string SubTitle, string Description, string Image, string isbn, int Books_Id)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.Description = Description;
+            this.Image = Image;
+            this.isbn = isbn;
+            this.Books_Id = Books_Id;
+        }
+    }
+}
---
+++ Reflection/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Reflection
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            DataSet dataSet = new DataSet("libro");
+            dataSet.ReadXml("sample.xml");
+
+            DataRow row = dataSet.Tables["book"].Rows[0];
+
+            // Reflection
+            Type bookType = typeof(Book);
+            object[] arguments = new object[] { typeof(string), typeof(string), typeof(string), typeof(string) };
+
+            Book result = Activator.CreateInstance(bookType, row.ItemArray) as Book;
+
+
+            Console.WriteLine("Tablas: {0}", result.Title);
+
+            dataSet.Dispose();
+        }
+    }
+}
